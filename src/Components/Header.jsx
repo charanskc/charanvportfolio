419ea7f145441b1de511a94790f9b853af26d2f0
Sai Charan <charanskc7@gmail.com>
2024-07-07 03:07:58 +0530

    added all the new changes such as dark theme and media screen for mainbody

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import "./Header.css";
+import DarkMode from './Darkmode';
+
 
 function Header() {
   return (
     <div className="header__container">
         <h4 className="header__title">Sai Charan</h4>
+        <DarkMode />
     </div>
   )
 }
